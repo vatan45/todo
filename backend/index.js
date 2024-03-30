6b75fc760ad44434912a54f949002453967d608f
vatanmalik45 <vmro45@gmail.com>
2024-03-30 12:53:15 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,15 @@ app.use(express.json());
 
 
 app.post("/todos", function(req, res){
+    const createPayload = req.body;
+    const parsedpayload = createTodo.safeParse(createPayload);
+    if(!parsedpayload.success){
+        res.status(411).json({
+            msg: "you sent the wrong input"
+        })
+        return;
+    }
+    //put it in mongodb
 
 
 })
@@ -15,6 +24,14 @@ app.get("/todos", function(req, res){
 })
 
 app.put("/complete", function(req, res){
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success){
+        res.status(441).json({
+            msg: "you sent the wrong input",
+        })
+        return;
+    }
 
 
 })
